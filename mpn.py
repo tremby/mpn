1f5c8275be97a9a1e5ce25e7226fe80816308392
Olivier Schwander <olivier.schwander@ens-lyon.org>
2010-06-04 07:42:59 -0700

    update from Walther Maldonado
    Ignore-this: 465a0e04124937fdbb92094fb9b91714
    - Used a single notify object rather than creating a new one on song-change, net result: Quickly changing songs will cause the popup contents to update, rather than causing multiple popup windows to pile up.
    - Added -p / --persist switch, causes the program to not abort when connection to MPD is lost, it will continue trying until reconnection (it will always exit if it cannot connect at start-up, though). Additionally added the correct signal handling to gracefully exit when the connection to the server is lost (previous versions would simply print a stack-trace and the program would not work anymore, without exiting)
    - Enabled user-configurable formatting for the title-string and body-string of the notify via the -f/--format and -F/--header switches, even though it might be easier to just edit the default near the top of the file.
    - Added support for reading more types of data from the current song: filename, track number, playlist position.
    - I changed space usage to tabs (I can't stand code which uses spaces rather than tabs for formatting)
    - Changed the remaining text to English (because a mix of English/French is not very professional looking)
    
    darcs-hash:20100604144259-919b6-dde61245e0b32f44d5208d8b1f51137207ba6ec7.gz

--- mpn.py
+++ mpn.py
@@ -27,185 +27,318 @@
 """Simple libnotify notifier for mpd"""
 
 import os, sys, cgi
-from optparse import OptionParser
+from optparse import Option, OptionParser, OptionGroup, SUPPRESS_HELP
 
 import gobject
 import gtk
 import mpd
 import pynotify
+import re
+
+format_title = "%t"
+format_body = "<b>%b</b><br><i>%a</i><br>(%d)"
 
 def convert_time(raw):
-    """Format a number of seconds to the hh:mm:ss format"""
-    # Converts raw time to 'hh:mm:ss' with leading zeros as appropriate
-
-    hour, minutes, sec = ['%02d' % c for c in (raw/3600,
-                                               (raw%3600)/60, raw%60)]
-
-    if hour == '00':
-        if minutes.startswith('0'):
-            minutes = minutes[1:]
-        return minutes + ':' + sec
-    else:
-        if hour.startswith('0'):
-            hour = hour[1:]
-        return hour + ':' + minutes + ':' + sec
+	"""Format a number of seconds to the hh:mm:ss format"""
+	# Converts raw time to 'hh:mm:ss' with leading zeros as appropriate
+        
+	hour, minutes, sec = ['%02d' % c for c in (raw/3600,
+	(raw%3600)/60, raw%60)]
+        
+	if hour == '00':
+		if minutes.startswith('0'):
+			minutes = minutes[1:]
+		return minutes + ':' + sec
+	else:
+		if hour.startswith('0'):
+			hour = hour[1:]
+		return hour + ':' + minutes + ':' + sec
 
 class Notifier:
-    "Main class for mpn"
-    debug = False
-    refresh_time = 500 # in ms
-    notify_timeout = 0 # in ms
-    host = ""
-    mpd = None
-    status = None
-    current = None
-    iterate_handler = None
+	"Main class for mpn"
+	debug = False
+	keys = False
+	persist = False
+	refresh_time = 750 # in ms
+	host = "localhost"
+	port = 6600
+	mpd = None
+	status = None
+	current = None
+	notifier = None
+	iterate_handler = None
+	title_txt = None
+	body_txt = None
+	re_t = re.compile('(%t)', re.S) #Title
+	re_a = re.compile('(%a)', re.S) #Artist
+	re_b = re.compile('(%b)', re.S) #alBum
+	re_d = re.compile('(%d)', re.S) #song Duration
+	re_f = re.compile('(%f)', re.S) #File
+	re_n = re.compile('(%n)', re.S) #track Number
+	re_p = re.compile('(%p)', re.S) #playlist Position
+        
+	def get_host(self):
+		"""get host name from MPD_HOST env variable"""
+		host = os.environ.get('MPD_HOST', 'localhost')
+		if '@' in host:
+			return host.split('@', 1)
+		return host
+        
+	def get_port(self):
+		"""get host name from MPD_PORT env variable"""
+		return os.environ.get('MPD_PORT', 6600)
+        
+	def get_title(self, safe=False):
+		"""Get the current song title"""
+		try:
+			title = self.current["title"]
+		except KeyError:
+			try:
+				title = self.current["file"]
+			except KeyError:
+				title = "???"
+		if self.debug:
+			print "Titre : " + title
+		if safe:
+			return cgi.escape(title)
+		return title
+        
+	def get_time(self, elapsed=False):
+		"""Get current time and total length of the current song"""
+		time = self.status["time"]
+		now, length = [int(c) for c in time.split(':')]
+		now_time = convert_time(now)
+		length_time = convert_time(length)
+                
+		if self.debug:
+			print "Position : " + now_time + " / " + length_time
+		if elapsed:
+			return now_time
+		return length_time
+        
+	def get_tag(self, tag, safe=False):
+		"""Get a generic tag from the current data"""
+		try:
+			data = self.current[tag]
+		except KeyError:
+			data = ""
+		if self.debug:
+			print tag + ": " + album
+		if safe:
+			return cgi.escape(data)
+		return data
+        
+	def get_file(self, safe=False):
+		"""Get the current song file"""
+		try:
+			file = self.current["file"]
+			# Remove left-side path
+			file = re.sub(".*"+os.sep, "", file)
+			# Remove right-side extension
+			file = re.sub("(.*)\..*", "\\1", file)
+		except KeyError:
+			file = ""
+		if self.debug:
+			print "Filename: " + file
+		if safe:
+			return cgi.escape(file)
+		return file
+        
+	def press_prev(notification=None, action=None, data=None):
+		if self.debug:
+			print "Previous song"
+		self.mpd.previous()
+        
+	def press_next():
+		if self.debug:
+			print "Next song"
+		self.mpd.next()
+        
+	def connect(self):
+		try:
+			self.mpd.connect(self.host, self.port)
+			return True
+		except mpd.socket.error:
+			return False
+		# Already connected
+		except mpd.ConnectionError:
+			return True
+        
+	def disconnect(self):
+		try:
+			self.mpd.disconnect()
+			return True
+		except mpd.socket.error:
+			return False
+		except mpd.ConnectionError:
+			return False
+        
+	def notify(self):
+		"""Display the notification"""
+		try:
+			self.status = self.mpd.status()
+                        
+			# only if there is a song currently playing
+			if not self.status["state"] in ['play', 'pause']:
+				if self.debug:
+					print "No files playing on the server." + self.host
+				return True
+                        
+			# only if the song has changed
+			new_current = self.mpd.currentsong()
+			if self.current == new_current:
+				return True
+			self.current = new_current
+                        
+			title = self.title_txt
+			body = self.body_txt
+			# get values with the strings html safe
+			title = self.re_t.sub(self.get_title(True), title)
+			title = self.re_f.sub(self.get_file(True), title)
+			title = self.re_d.sub(self.get_time(), title)
+			title = self.re_a.sub(self.get_tag('artist', True), title)
+			title = self.re_b.sub(self.get_tag('album', True), title)
+			title = self.re_n.sub(self.get_tag('track'), title)
+			title = self.re_p.sub(self.get_tag('pos'), title)
+                        
+			body = self.re_t.sub(self.get_title(True), body)
+			body = self.re_f.sub(self.get_file(True), body)
+			body = self.re_d.sub(self.get_time(), body)
+			body = self.re_a.sub(self.get_tag('artist', True), body)
+			body = self.re_b.sub(self.get_tag('album', True), body)
+			body = self.re_n.sub(self.get_tag('track'), body)
+			body = self.re_p.sub(self.get_tag('pos'), body)
+		except mpd.ConnectionError, (ce):
+		# Ugly, but there's no mpd.isconnected() method
+			self.disconnect()
+			if self.persist:
+				self.connect()
+				return True
+			else:
+				print "Lost connection to server, exiting..."
+				sys.exit(1)
+                
+		# set paramaters and display the notice
+		if self.debug:
+			print "Title string: " + title
+			print "Body string: " + body
+		self.notifier.update(title, body)
+		if self.keys:
+			self.notifier.add_action("clicked", "&lt;&lt;", self.press_prev, None)
+			self.notifier.add_action("clicked", ">>", self.press_next, None)
+		if not self.notifier.show():
+			print "Impossible to display the notification"
+			return False
 
-    def get_host(self):
-        """get host name from MPD_HOST env variable"""
-        host = os.environ.get('MPD_HOST', 'localhost')
-        if '@' in host:
-            return host.split('@', 1)
-        return host
-
-    def get_time(self):
-        """Get current time and total lentght of the current song"""
-        time = self.status["time"]
-        now, length = [int(c) for c in time.split(':')]
-        now_time = convert_time(now)
-        length_time = convert_time(length)
-
-        if self.debug:
-            print "Position : " + now_time + " / " + length_time
-        return (now_time, length_time)
-
-    def get_title(self):
-        """Get the current song title"""
-        try:
-            title = self.current["title"]
-        except KeyError:
-            title = "???"
-            print "<Pas de titre trouvé>"
-        if self.debug:
-            print "Titre : " + title
-        return title
-
-    def get_album(self):
-        """Get the current song album"""
-        try:
-            album = self.current["album"]
-        except KeyError:
-            album = "???"
-            print "<Pas d'album trouvé>"
-        if self.debug:
-            print "Album : " + album
-        return album
-
-    def get_artist(self):
-        """Get the current song artist"""
-        try:
-            artist = self.current["artist"]
-        except KeyError:
-            artist = "???"
-            print "<Pas d'artiste trouvé>"
-        if self.debug:
-            print "Artiste : " + artist
-        return artist
-
-    def notify(self):
-        """Display the notification"""
-        self.status = self.mpd.status()
-
-        # only if there is a song currently playing
-        if not self.status["state"] in ['play', 'pause']:
-            if self.debug:
-                print "Pas de lecture en cours sur le serveur " + self.host
-            return True
-
-        # only if the song has changed
-        new_current = self.mpd.currentsong()
-        if self.current == new_current:
-            return True
-        self.current = new_current
-
-        # get values and make the strings html safe
-        album = cgi.escape(self.get_album())
-        title = cgi.escape(self.get_title())
-        artist = cgi.escape(self.get_artist())
-        _, length = self.get_time()
-
-        if self.debug:
-            print ""
-
-        body = "de <b>" + artist + "</b> in <i>" + album + \
-               "</i>\nDurée : " + length
-
-        # set paramaters and display the notice
-        notif = pynotify.Notification(title, body)
-        notif.set_timeout(self.notify_timeout)
-        if not notif.show():
-            print "<Impossible d'afficher la notification>"
-            return False
-
-        return True
-
-    def run(self):
-        """Launch the iteration"""
-        self.iterate_handler = gobject.timeout_add(self.refresh_time,
-                                                   self.notify)
-
-    def __init__(self, debug=False, notify_timeout=3):
-        """Initialisation of mpd client and pynotify"""
-        self.debug = debug
-        # param notify_timeout is in seconds
-        self.notify_timeout = 1000 * notify_timeout
-        self.host = self.get_host()
-
-        try:
-				self.mpd = mpd.MPDClient();
-				self.mpd.connect(self.host, 6600);
-        except mpd.socket.error:
-            print "Impossible de se connecter au serveur " + self.host
-            sys.exit(1)
-
-        pynotify.init('mpn')
+		return True
+        
+	def run(self):
+		"""Launch the iteration"""
+		self.iterate_handler = gobject.timeout_add(self.refresh_time, self.notify)
+        
+	def close(self):
+		return self.disconnect()
+        
+	def __init__(self, debug=False, notify_timeout=3, show_keys=False,
+		persist=False, title_format=None, body_format=None):
+		"""Initialisation of mpd client and pynotify"""
+		self.debug = debug
+		self.keys = show_keys
+		self.persist = persist
+		self.notifier = pynotify.Notification("Song title", "Song details")
+		# param notify_timeout is in seconds
+		if notify_timeout == 0:
+			self.notifier.set_timeout(pynotify.EXPIRES_NEVER)
+		else:
+			self.notifier.set_timeout(1000 * notify_timeout)
+                
+		self.title_txt = re.sub("<br>", "\n", title_format)
+		self.body_txt = re.sub("<br>", "\n", body_format)
+                
+		if self.debug:
+			print "Title format: " + self.title_txt
+			print "Body format: " + self.body_txt
+		self.host = self.get_host()
+		self.port = self.get_port()
+		self.mpd = mpd.MPDClient()
+		if not self.connect():
+			print "Impossible to connect to server " + self.host
+			sys.exit(1)
+                
+		pynotify.init('mpn')
 
 if __name__ == "__main__":
-    # initializate the argument parser
-    PARSER = OptionParser()
+	# initializate the argument parser
+	PARSER = OptionParser()
+        
+	# help/debug mode
+	PARSER.add_option("--debug", action="store_true", dest="debug",
+		default=False, help="Turn on debugging information")
+        
+	# does mpn will fork ?
+	PARSER.add_option("-d", "--daemon", action="store_true", dest="fork",
+		default=False, help="Fork into the background")
+        
+	PARSER.add_option("-p", "--persist", action="store_true", dest="persist",
+		default=False, help="Do not exit when connection fails")
+        
+	# how many time the notice will be shown
+	PARSER.add_option("-t", "--timeout", type="int", dest="timeout", default=3,
+		help="Notification timeout in secs (use 0 to disable)")
+        
+	# whether to print updates on all song changes
+	PARSER.add_option("-k", "--keys", action="store_true", dest="keys",
+	# If only there were better documentation on how to use libnotify's buttons
+#		default=False, help="Add Prev/Next buttons to notify window")
+		default=False, help=SUPPRESS_HELP)
+        
+	# whether to print updates on all song changes
+	PARSER.add_option("-o", "--once", action="store_false", dest="repeat",
+		default=True, help="Notify once and exit")
+        
+	# Format strings
+	GROUP = OptionGroup(PARSER, "Format related options for the notify display",
+		"Supported wildcards:"
+		" %t title /"
+		" %a artist /"
+		" %b album /"
+		" %d song duration /"
+		" %f base filename /"
+		" %n track number /"
+		" %p playlist position /"
+		" <i> </i> italic text /"
+		" <b> </b> bold text /"
+		" <br> line break")
+        
+	GROUP.add_option("-F", "--header", dest="title_format", default=format_title,
+		help="Format for the notify header (default: %default)")
+        
+	GROUP.add_option("-f", "--format", dest="body_format", default=format_body,
+		help="Format for the notify body (default: %default)")
+        
+	PARSER.add_option_group(GROUP)
+        
+	# parse the commandline
+	(OPTIONS, ARGS) = PARSER.parse_args()
+        
+	# initializate the notifier
+	MPN = Notifier(debug=OPTIONS.debug, notify_timeout=OPTIONS.timeout,
+		show_keys=OPTIONS.keys, persist=OPTIONS.persist,
+		title_format=OPTIONS.title_format, body_format=OPTIONS.body_format)
+        
+	# fork if necessary
+	if OPTIONS.fork and not OPTIONS.debug:
+		if os.fork() != 0:
+			sys.exit(0)
+        
+	# run the notifier
+	if OPTIONS.repeat:
+		try:
+			MPN.run()
+			gtk.main()
+		except KeyboardInterrupt:
+			MPN.close()
+			sys.exit(0)
+	else:
+		MPN.notify()
 
-    # help/debug mode
-    PARSER.set_defaults(debug=False)
-    PARSER.add_option("--debug", action="store_true", dest="debug", help="Turn on debugging information")
-
-    # does mpn will fork ?
-    PARSER.add_option("-d", "--daemon", action="store_true", dest="fork", help="Fork into the background")
-    PARSER.add_option("-n", "--nodaemon", action="store_false", dest="fork", help="Do not daemonize into background (default)")
-    PARSER.set_defaults(fork=False)
-
-    # how many time the notice will be shown
-    PARSER.set_defaults(timeout=3)
-    PARSER.add_option("-t", "--timeout", type="int", action="store", dest="timeout", help="Notification timeout in secs")
-
-    # whether to print updates on all song changes
-    PARSER.add_option("-o", "--once", action="store_false", dest="repeat", help="Notify once and exit")
-    PARSER.set_defaults(repeat=True)
-
-
-    # parse the commandline
-    (OPTIONS, ARGS) = PARSER.parse_args()
-
-    # initializate the notifier
-    MPN = Notifier(debug=OPTIONS.debug, notify_timeout=OPTIONS.timeout)
-
-    # fork if necessary
-    if OPTIONS.fork and not OPTIONS.debug:
-        if os.fork() != 0:
-            sys.exit(0)
-
-    # run the notifier
-    if OPTIONS.repeat:
-        MPN.run()
-        gtk.main()
-    else:
-        MPN.notify()
