b4f84cf879d7a90fc2dfe89ad0dc617ceb1cf54b
Olivier Schwander <olivier.schwander@ens-lyon.org>
2011-04-16 10:31:41 -0700

    add a setup.py script
    Ignore-this: c5215600b6f72e8fb96861ceeb3be957
    
    darcs-hash:20110416173141-919b6-694d3b4fb5be7d7f6e470bf840e1399e5c40414a.gz

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name             = 'MPNotifier',
+      version          = '1.0',
+      description      = 'A lightweigh notifier for MPD',
+      long_description = long_description,
+      author           = 'Olivier Schwander',
+      author_email     = 'olivier.schwander@chadok.info',
+      url              = 'http://chadok.info/mpn',
+      scripts          = ['mpn'],
+      classifiers  = [
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Sound/Audio',
+        ],
+      requires = [
+        "pyyaml",
+        "pygtk",
+        "mpd",
+        "pynotify"
+        ],
+
+     )
+
